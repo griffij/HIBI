e0a01e7388d083f81898a2fb9cf51c3b98e53cf8
Geoscience Australia <jono.d.griffin@gmail.com>
2022-02-14 13:27:12 +1100

    Better plot of locations

--- plotting/plot_posterior.R
+++ plotting/plot_posterior.R
@@ -11,13 +11,26 @@ library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(ggthemes)
+library(dplyr)
 #library(rgeos)
 
-
+event = 'Beachport_1897'
 posterior_file = c('../outputs/df_posterior.csv')
 df_post = read.csv(posterior_file)
+mmi_datafile = '../data/1897-beachport-ssm.txt'
+data = read.csv(mmi_datafile, header=TRUE, sep='\t')
 
-figname = 'plots/posterior_mw.png'  
+# Get data without NA coordinates
+data = filter(data, (data$Latitude !="-NA-") & (data$MMI !="F") & (data$MMI !="L"))
+
+lat_ip = as.numeric(data$Latitude)
+lon_ip = as.numeric(data$Longitude)
+mmi = as.numeric(data$MMI)
+
+mmi_data = data_frame(lat_ip, lon_ip, mmi)
+
+
+figname = paste0('plots/posterior_mw_', event, '.png')
 png(figname, units="in", width=6, height=6, res=300)     
 print("Mean mw")
 print(mean(df_post$mw))
@@ -25,7 +38,7 @@ mw_percentiles = quantile(df_post$mw, probs = c(0.025, 0.26, 0.5, 0.84, 0.975))
 print("0.025, 0.26, 0.5, 0.84, 0.975")
 print(mw_percentiles)
 d = density(df_post$mw)
-plot(d)
+plot(d, main='1897 Beachport', xlab='Mw', ylab='Density')
 dev.off()
 
 
@@ -39,32 +52,43 @@ contour_95 =with(kd, contourLines(x=eval.points[[1]], y=eval.points[[2]],
 	       
 contour_95 = data.frame(contour_95)
 contour_25 = with(kd, contourLines(x=eval.points[[1]], y=eval.points[[2]],
-	   z=estimate, levels=cont["25%"])[[1]])
+	   z=estimate, levels=cont["75%"])[[1]])
 contour_25 = data.frame(contour_25) 
 contour_50 = with(kd, contourLines(x=eval.points[[1]], y=eval.points[[2]],
 	   z=estimate, levels=cont["50%"])[[1]])
 contour_50 = data.frame(contour_50)   
 
-world <- ne_countries(scale = "medium", returnclass = "sf")
+world <- ne_countries(country='australia', scale = "large", returnclass = "sf")
 
-figname = 'plots/posterior_location.png'
+figname = paste0('plots/posterior_location_', event, '.png')
+png(figname, units="in", width=6, height=6, res=300)
 
 #print(loc_data)
 print(length(lon_source))
 print(length(lat_source))
 print(dim(loc_data))
 #ggplot(df_post, aes(x=df_post$lon_source, y=df_post$lat_source)) +
-ggplot(loc_data, aes(x=lon_source, y=lat_source)) + 
+#ggplot(loc_data, aes(x=lon_source, y=lat_source)) + 
+ggplot() +
 #  geom_tile(data = d, aes(x = lon, y = lat, fill=x)) +
 #  geom_sf(data = world, fill=alpha("lightgrey", 0), color="lightgrey") +
-  stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
-  geom_path(aes(x=x, y=y), data=contour_95, lwd=0.5) +
-  geom_path(aes(x=x, y=y), data=contour_50, lwd=0.5) +
-  geom_path(aes(x=x, y=y), data=contour_25, lwd=0.5) +
+#  geom_sf(data = world,  fill=alpha("lightgrey", 0), color="lightgrey") + 
+  stat_density_2d(data=loc_data, aes(x=lon_source, y=lat_source, fill = ..density..), geom = "raster", contour = FALSE, h=c(0.05,0.05), n=500) +
   scale_fill_distiller(palette="Greys", direction=1) +
-  labs(colour = "Density") #+
-#  geom_sf(data = world) #, fill=alpha("lightgrey"), color="lightgrey") #+
-#  coord_sf(xlim=c(130,150), ylim=c(-40,-30)) + 
-#  theme_bw()
+  labs(colour = "Density") +
+  geom_path(aes(x=x, y=y), data=contour_95, lwd=0.15) +
+  geom_path(aes(x=x, y=y), data=contour_50, lwd=0.3) +
+  geom_path(aes(x=x, y=y), data=contour_25, lwd=0.5) +
+  geom_sf(data = world,  fill=alpha("lightgrey", 0), color="black", lwd=0.3) +
+  geom_point(data = mmi_data, aes(x=lon_ip, y=lat_ip, color=mmi)) +
+  scale_colour_distiller(palette='RdYlGn', direction=-1, limits=c(0,8)) +
+  labs(colour = "Intensity") +
+#  geom_sf(data = world,  fill=alpha("lightgrey", 0), color="black", lwd=0.3) +
+  coord_sf(xlim=c(139,142), ylim=c(-38.5,-35.5)) +
+  xlab('Longitude') +
+  ylab('Latitude') +
+  ggtitle('1897 Beachport') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_bw()
 
 dev.off()
