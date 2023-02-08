9be6bec2e1b426f6834e7334bf66c71e67410776
Geoscience Australia <jono.d.griffin@gmail.com>
2023-02-09 10:09:05 +1100

    Adding details for additional event

--- jags/run_aus_ipe_allen.R
+++ jags/run_aus_ipe_allen.R
@@ -10,25 +10,59 @@ paramfile = '../params/1897-beachport-params.txt'
 
 #datafile = '../data/1897-beachport-ssm.txt'
 #datafile = '../data/1902-warooka-ssm.txt'
-datafile = '../data/1954-adelaide-nehrp.txt' 
+#datafile = '../data/1954-adelaide-nehrp.txt'
+#datafile = '../data/1954-adelaide-aees.txt'  
+datafile = '../data/1918-QLD-updated.txt'
 #name = '1902_warooka'
-name = '1954_Adelaide'
+#name = '1954_Adelaide_aees'
+name = '1918_QLD'
+weight_data = FALSE#TRUE # Flag for whether to weight data based on quality index
 data = read.csv(datafile, header=TRUE, sep='\t')
 print(data)
 #lats = data$Latitude
 # Get data without NA coordinates
+# Change EMS to MMI in column name
+colnames(data)[3] = 'MMI'
 data = filter(data, (data$Latitude !="") & (data$Latitude !="-NA-") & (data$Latitude !="-na-") &
      (data$MMI !="F") & (data$MMI !="L"))
 
 lat_ip = as.numeric(data$Latitude)
 lon_ip = as.numeric(data$Longitude)
 mmi = as.numeric(data$MMI)
+quality = data$Err
 print(lat_ip)
 print(lon_ip)
 print(mmi)
+weights = rep(1, length(mmi))
+if(weight_data){
+	print('Weighting data based on quality index')
+	for(i in 1:length(quality)){
+		if(quality[i]=="A"){
+			weights[i] = 1.#4
+#			lat_ip = c(lat_ip, lat_ip[i], lat_ip[i], lat_ip[i]) # Weight by four for A quality
+#			lon_ip = c(lon_ip, lon_ip[i], lon_ip[i], lon_ip[i])
+#			mmi = c(mmi, mmi[i], mmi[i], mmi[i])
+		} else if(quality[i]=="B"){
+		        weights[i] = 0.75#3
+#		        lat_ip = c(lat_ip, lat_ip[i], lat_ip[i]) # Weight by three for B quality
+#			lon_ip = c(lon_ip, lon_ip[i], lon_ip[i])
+#			mmi = c(mmi, mmi[i], mmi[i])
+		} else if(quality[i]=="C"){
+		        weights[i] = 0.5#2
+#		        lat_ip = c(lat_ip, lat_ip[i]) # Weight by two for C quality, by one for D quality
+#			lon_ip = c(lon_ip, lon_ip[i])
+#			mmi = c(mmi, mmi[i])
+		} else {weights[i] = 0.25
+		}
+		}}
+mmi
+weights
 N = length(mmi)
+print(quality)
 print(N)
-sim.data.jags = list("mmi", "lon_ip", "lat_ip", "N")
+print(lon_ip)
+sim.data.jags = list("mmi", "lon_ip", "lat_ip", "N", "weights")
+
 
 # Define the parameters whose posterior distributions we want to calculate
 bayes.mod.params <- c("lon_source", "lat_source", "dep", "mw")
